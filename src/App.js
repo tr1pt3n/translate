af0b6f7200734b88e871458c58934772dab37ba7
tr1pt3n <nguyenhoangtuan069@gmail.com>
2023-01-13 13:36:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import i18n, { changeLanguage } from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        // the translations
+        // (tip move them in a JSON file and import them,
+        // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+        resources: {
+            vi: {
+                translation: {
+                    "Chào mừng đến với React": "Chào mừng đến với React",
+                    "Tôi tên là ABC": "Tôi tên là ABC",
+                    "Tôi đến từ Việt Nam": "Tôi đến từ Việt Nam",
+                },
+            },
+            en: {
+                translation: {
+                    "Chào mừng đến với React": "Welcome to React",
+                    "Tôi tên là ABC": "My name is ABC",
+                    "Tôi đến từ Việt Nam": "I come from Vietnam",
+                },
+            },
+        },
+        lng: localStorage.getItem('lng') ||"en", // if you're using a language detector, do not define the lng option
+        fallbackLng: "vi",
+
+        interpolation: {
+            escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+        },
+    });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { t } = useTranslation();
+    const handleChangeLng = (lng) => {
+      changeLanguage(lng); 
+      localStorage.setItem("lng", lng);
+    }
+
+    return (
+        <div>
+            <button onClick={() => handleChangeLng('vi')}>Vietnam</button>
+            <button onClick={() => handleChangeLng('en')}>English</button>
+            <h2>{t("Chào mừng đến với React")}</h2>
+            <p>{t("Tôi tên là ABC")}</p>
+            <p>{t("Tôi đến từ Việt Nam")}</p>
+        </div>
+    );
 }
 
 export default App;
